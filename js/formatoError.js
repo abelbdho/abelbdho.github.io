d1153d7cf5466fd6b5d9e5202f0a1252b435f361
abelbdho <abelbdho@uma.es>
2019-06-22 17:04:09 +0200

    Mitad cambios antonio

--- js/formatoError.js
+++ js/formatoError.js
@@ -1,6 +1,24 @@
+var cont = 0;
 function formato(campo, obj) 
    {
-     if (campo.value == parseInt(campo.value, 10)){
+     if (campo.value == parseInt(campo.value, 10) && cont == 0){
+		 cont++;
+		var div = document.createElement('div');
+		div.setAttribute('class', 'avisoOblig');
+		div.innerHTML = document.getElementById("blockOfStuffX").innerHTML;
+		document.getElementById(obj).appendChild(div);
+	}
+    }; 
+    
+var cont2 = 0;
+function formato2(campo, obj) 
+   {
+	   
+	 if(typeof campo === 'string'){
+		 campo = document.getElementById(campo);
+		 }
+     if (campo.value.length == 0 && cont2 == 0){
+		 cont2++;
 		var div = document.createElement('div');
 		div.setAttribute('class', 'avisoOblig');
 		div.innerHTML = document.getElementById("blockOfStuffX").innerHTML;
